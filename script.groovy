92df6cd6a6fdc10f120970ca46cb60315fb78083
Adrian B <adrian.borzym@int.pl>
2023-03-08 15:41:32 +0100

    add gv scripts

--- script.groovy
+++ script.groovy
@@ -1,9 +1,14 @@
-def buildApp() {
-    echo 'building the application'
+def buildJar() {
+    echo "Building jar file..."
+    sh 'mvn package'
 }
 
-def testApp() {
-    echo 'testing the application'
+def pushImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-cred', passwordVariable: 'PASS', usernameVariable: 'USER'  )])
+        sh 'docker build -t aborzymdocker/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push aborzymdocker/demo-app:jma-2.0'
 }
 
 def deployApp(){
